9491485b4a174e6251903e1d930c2bc7c9bb194e
rahul488 <roshon7894@gmail.com>
2020-03-27 10:35:05 +0530

    Hackerrank Solution

---
+++ dayOfProgrammer/day_of_programmer.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the dayOfProgrammer function below.
+    static String dayOfProgrammer(int year) 
+    {
+        int february_days=28;
+        if(year >=1919)
+        {
+            if(year % 4==0)
+            {
+                if(year % 100!=0)
+                {
+                   february_days++;
+                }
+                else if(year % 100 == 0)
+                {
+                    if(year % 400 == 0)
+                    {
+                        february_days++;
+                    }
+                }
+
+            }
+        }
+        else if(year >= 1700 && year <=1917)
+        {
+            if(year % 4 == 0)
+            {
+                february_days++;
+            }
+        }
+        else if(year == 1918)
+        {
+            february_days-=13;
+        }
+        int date=215+february_days;
+        String day_of_programmer=256-date+"."+"09"+"."+year;
+        return  day_of_programmer;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int year = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = dayOfProgrammer(year);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
