a5764b369f7d0377e4980e1a6d1e748ad32b9970
rahul488 <roshon7894@gmail.com>
2020-03-27 12:06:57 +0530

    DrawingBook_HackerrankSolution

---
+++ Drawing/Drawing.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int n, int p) {
+        
+
+        int total_pageCount=n/2;
+        int target_pageCount_front=p/2;
+        int target_pageCount_back=total_pageCount-target_pageCount_front;
+        return Math.min(target_pageCount_front,target_pageCount_back);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
